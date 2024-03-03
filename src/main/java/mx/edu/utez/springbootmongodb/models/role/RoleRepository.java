7ad0237b112ce44c01f907e7a17f1b891b007e7a
Leo-Noriega <leonoriega100@gmail.com>
2024-03-02 20:23:35 -0600

    Login finalizado con Token JWT y asignación de Role a usuarios

---
+++ src/main/java/mx/edu/utez/springbootmongodb/models/role/RoleRepository.java
@@ -0,0 +1,11 @@
+package mx.edu.utez.springbootmongodb.models.role;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends MongoRepository<Role, String> {
+    Optional<Role> findByName(String name);
+}

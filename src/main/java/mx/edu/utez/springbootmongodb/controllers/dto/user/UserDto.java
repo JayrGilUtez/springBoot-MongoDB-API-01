22d63a33ac42e245c5cf21c3f09308c9f4aa49ea
Leo-Noriega <leonoriega100@gmail.com>
2024-03-07 22:47:21 -0600

    Feature: change password added

--- src/main/java/mx/edu/utez/springbootmongodb/controllers/dto/user/UserDto.java
+++ src/main/java/mx/edu/utez/springbootmongodb/controllers/dto/user/UserDto.java
@@ -1,4 +1,15 @@
 package mx.edu.utez.springbootmongodb.controllers.dto.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
+    private String mail;
+    private String password;
 }

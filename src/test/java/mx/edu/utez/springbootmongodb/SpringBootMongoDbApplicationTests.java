abc10dbaae21251d56eae3a254c8ea19fc71190c
Galicia_Jimenez_Jayr_Gil <20223tn052@utez.edu.mx>
2024-03-25 19:22:21 -0600

    fix(Test): test remove from MongoDbTestApplicationTests
    testGetLevel() was duplicated so it was just removed from MongoDbTestApplicationTests file.

--- src/test/java/mx/edu/utez/springbootmongodb/SpringBootMongoDbApplicationTests.java
+++ src/test/java/mx/edu/utez/springbootmongodb/SpringBootMongoDbApplicationTests.java
@@ -1,20 +1,10 @@
 package mx.edu.utez.springbootmongodb;
 
-import mx.edu.utez.springbootmongodb.services.TrashcanService;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SpringBootMongoDbApplicationTests {
 
-    @Test
-    void testGetLevel() {
-        TrashcanService trashcanService = new TrashcanService(null, null);
-        Double distance = 12.0; // Distancia detectada
-        Double expectedLevel = 13.3; // Porcetnaje esperado
-        assertEquals(expectedLevel, trashcanService.getLevel(distance));
-    }
 
 }

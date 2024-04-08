c076899e83e94609244921a9e8c9245ca0b26623
Galicia_Jimenez_Jayr_Gil <20223tn052@utez.edu.mx>
2024-04-08 14:50:32 -0600

    feat(properties, MongoConfig): Connection to mongoDB Atlas confing added.
    Connection to mongoDB Atlas confing was added and some methods wete added to MongoConfig in order to avoid problems creating the MongoTemplate Bean.
    
    Author: Jayr_Gil

--- src/main/java/mx/edu/utez/springbootmongodb/config/MongoConfig.java
+++ src/main/java/mx/edu/utez/springbootmongodb/config/MongoConfig.java
@@ -1,5 +1,6 @@
 package mx.edu.utez.springbootmongodb.config;
 
+import com.mongodb.ConnectionString;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,33 +8,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
-
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
-    @Value("${spring.data.mongodb.database}")
-    private String database;
-
-    @Value("${spring.data.mongodb.host}")
-    private String host;
-
-    @Value("${spring.data.mongodb.port}")
-    private int port;
+    @Value("${spring.data.mongodb.uri}")
+    private String uri;
 
     @Override
     protected String getDatabaseName() {
-        return database;
+        return new ConnectionString(uri).getDatabase();
     }
 
     @Override
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb://" + host + ":" + port);
+        return MongoClients.create(uri);
     }
 
     @Bean
     public MongoTemplate mongoTemplate() throws Exception {
         return new MongoTemplate(mongoClient(), getDatabaseName());
     }
-
-
 }
